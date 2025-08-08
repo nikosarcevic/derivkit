f5b299544feab38b2e82095568f3816c9b77a913
niko.sarcevic <nikolina.sarcevic@gmail.com>
2025-08-07 20:19:40 -0400

    Improve stability for scalar/vector output in ForecastKit and cleanup adaptive fit behavior

--- src/derivkit/forecast_kit.py
+++ src/derivkit/forecast_kit.py
@@ -1,5 +1,6 @@
-import numpy as np
 from copy import deepcopy
+
+import numpy as np
 
 from derivkit.kit import DerivativeKit
 
@@ -12,23 +13,24 @@ class ForecastKit:
     function : callable
         The scalar or vector-valued function to differentiate.
         For N observables with M model parameters, this should be a list of
-            N functions, with each one a function of 1 list of M parameters.
-    central_value : float
+        N functions, with each one a function of 1 list of M parameters.
+    central_value : array-like
         The point at which the derivative is evaluated.
+        This should be a list or array of M values, corresponding to M model parameters.
         For M model parameters, this should be 1 list of M values,
-            provided in the same order as the requested input of the functions
-            to be differentiated.
+        provided in the same order as the requested input of the functions
+        to be differentiated.
     covariance_matrix : float
         The covariance matrix of the observables.
         For N observables, this should be an NxN matrix, with the order of the 
-            matrix entries matching the order of the functions to be differentiated.
+        matrix entries matching the order of the functions to be differentiated.
     """
 
     def __init__(self, function, central_value, covariance_matrix):
         self.function = function
-        self.central_value = central_value
+        self.central_value = np.atleast_1d(central_value)
         self.covariance_matrix = covariance_matrix
-        self.M = len(central_value)
+        self.M = len(self.central_value)
         self.N = len(covariance_matrix)
 
     def get_derivatives(self, derivative_order):
@@ -56,16 +58,17 @@ class ForecastKit:
 
         if derivative_order == 1:
             # Get the first-order derivatives
-            first_order_derivatives = np.zeros((self.M,self.N))
+            first_order_derivatives = np.zeros((self.M, self.N))
             for m in range(self.M):
                 # 1 parameter to differentiate, and M-1 parameters to hold fixed
                 central_values_x = deepcopy(self.central_value)
-                functionToDiff = self._reduceFunctionTo1Var(self.function, m, central_values_x)
+                function_to_diff = self._get_partial_function(self.function, m, central_values_x)
                 # Currently done with adaptive
-                kit = DerivativeKit(functionToDiff, \
-                                    self.central_value[m], \
+                kit = DerivativeKit(function_to_diff,
+                                    self.central_value[m],
                                     derivative_order=1)
-                first_order_derivatives[m] = kit.adaptive.compute()
+                #first_order_derivatives[m] = kit.adaptive.compute()
+                first_order_derivatives[m] = np.atleast_1d(kit.adaptive.compute())
             return first_order_derivatives
 
         elif derivative_order == 2:
@@ -76,29 +79,35 @@ class ForecastKit:
                     if m1==m2:
                         # 1 parameter to differentiate twice, and M-1 parameters to hold fixed
                         central_values_x = deepcopy(self.central_value)
-                        functionToDiff1 = self._reduceFunctionTo1Var(self.function, m1, central_values_x)
-                        kit1 = DerivativeKit(functionToDiff1, \
-                                             self.central_value[m1], \
-                                             derivative_order = 2)
+                        function_to_diff1 = self._get_partial_function(self.function, m1, central_values_x)
+                        kit1 = DerivativeKit(function_to_diff1,
+                                             self.central_value[m1],
+                                             derivative_order=2)
                         # Currently done with adaptive
-                        second_order_derivatives[m1][m2] = kit1.adaptive.compute()
+                        #second_order_derivatives[m1][m2] = kit1.adaptive.compute()
+                        second_order_derivatives[m1][m2] = np.atleast_1d(kit1.adaptive.compute())
+
                     else:
                         # 2 parameters to differentiate once, with other parameters held fixed
-                        def functionToDiff2(y):
+                        def function_to_diff2(y):
                             central_values_y = deepcopy(self.central_value)
                             central_values_y[m2] = y
-                            functionToDiff1 = self._reduceFunctionTo1Var(self.function, m1, central_values_y)
-                            kit1 = DerivativeKit(functionToDiff1, \
-                                                 self.central_value[m1], \
-                                                 derivative_order = 1)
+                            function_to_diff1 = self._get_partial_function(self.function, m1, central_values_y)
+                            kit1 = DerivativeKit(function_to_diff1,
+                                                 self.central_value[m1],
+                                                 derivative_order=1)
                             # Currently done with adaptive
                             return kit1.adaptive.compute()
-                        kit2 = DerivativeKit(functionToDiff2, \
-                                             self.central_value[m2], \
-                                             derivative_order = 1)
+                        kit2 = DerivativeKit(function_to_diff2,
+                                             self.central_value[m2],
+                                             derivative_order=1)
                         # Currently done with adaptive
-                        second_order_derivatives[m1][m2] = kit2.adaptive.compute()
+                        #second_order_derivatives[m1][m2] = kit2.adaptive.compute()
+                        second_order_derivatives[m1][m2] = np.atleast_1d(kit2.adaptive.compute())
+
             return second_order_derivatives
+
+        raise RuntimeError("Unreachable code reached in get_forecast_tensors.")
 
     def get_forecast_tensors(self, forecast_order=1):
         """
@@ -129,51 +138,57 @@ class ForecastKit:
         # Invert the covariance matrix
         try:
             inverse_covariance_matrix = np.linalg.inv(self.covariance_matrix)
-        except:
-            print("Covariance inversion failed!")
-            inverse_covariance_matrix = np.empty((self.N,self.N))
-            inverse_covariance_matrix.fill(np.nan)
+        except np.linalg.LinAlgError:
+            print("Standard inversion failed. Trying pseudoinverse.")
+            try:
+                inverse_covariance_matrix = np.linalg.pinv(self.covariance_matrix)
+            except Exception as e:
+                print(f"Pseudoinverse also failed: {e}")
+                inverse_covariance_matrix = np.full((self.N, self.N), np.nan)
 
         if forecast_order == 1:
             # Compute Fisher matrix
-            dfp1 = self.get_derivatives(derivative_order = 1)
+            dfp1 = self.get_derivatives(derivative_order=1)
             # F_ab = sum(i,j) df_i/dp_a * InvCov_ij * df_j/dp_b
-            F_ab = np.einsum('ai,ij,bj->ab', dfp1, inverse_covariance_matrix, dfp1)
-            return F_ab
+            fisher_ab = np.einsum('ai,ij,bj->ab', dfp1, inverse_covariance_matrix, dfp1)
+            return fisher_ab
 
         elif forecast_order == 2:
             # Compute doublet-DALI tensors
-            dfp1 = self.get_derivatives(derivative_order = 1)
-            dfp2 = self.get_derivatives(derivative_order = 2)
+            dfp1 = self.get_derivatives(derivative_order=1)
+            dfp2 = self.get_derivatives(derivative_order=2)
             # G_abc = sum(i,j) df_i/(dp_a dp_b )* InvCov_ij * df_j/dp_c
-            G_abc = np.einsum('abi,ij,cj->abc', dfp2, inverse_covariance_matrix, dfp1)
+            g_abc = np.einsum('abi,ij,cj->abc', dfp2, inverse_covariance_matrix, dfp1)
             # H_abcd = sum(i,j) df_i/(dp_a dp_b) * InvCov_ij * df_j/(dp_c dp_d)
-            H_abcd = np.einsum('abi,ij,cdj->abcd', dfp2, inverse_covariance_matrix, dfp2)
-            return G_abc, H_abcd
+            h_abcd = np.einsum('abi,ij,cdj->abcd', dfp2, inverse_covariance_matrix, dfp2)
+            return g_abc, h_abcd
 
-    def _reduceFunctionTo1Var(self, someFunction, indexToVary, fixedParamValues):
+        raise RuntimeError("Unreachable code reached in get_forecast_tensors.")
+
+    def _get_partial_function(self, full_function, variable_index, fixed_values):
         """
-        Reduces a multi-variable function to a single-variable function,
-            which can then be passed to the DerivativeKit.
+        Returns a single-variable version of a multivariate function, where all
+        parameters except one are held fixed.
 
         Parameters
         ----------
-        someFunction : callable
-            The function to be reduced. The function should take one argument,
-                a single list of M variables.
-        indexToVary : int
-            The index of the parameter to be kept as the independent variable.
-        fixedParamValues : list
-            A list of floats, fiducial parameter values for the parameters 
-                which will be kept fixed.
+        full_function : callable
+            A function that takes a list of M parameters and returns a vector of N observables.
+        variable_index : int
+            Index of the parameter to treat as the variable.
+        fixed_values : list or np.ndarray
+            The list of parameter values to use as fixed inputs for all parameters
+            except the one being varied.
 
         Returns
         -------
-        function : callable
-            A function which takes a single float variable as its input.
+        callable
+            A function of a single variable, suitable for use in differentiation.
         """
-        # Reduces the multi-variable function to a single-variable function
-        def function1Var(x):
-            fixedParamValues[indexToVary] = x
-            return someFunction(fixedParamValues)
-        return function1Var
+
+        def partial_function(x):
+            params = deepcopy(fixed_values)
+            params[variable_index] = x
+            return np.atleast_1d(full_function(params))
+
+        return partial_function
