a2044554c005056cbfda3459aff3acab782195c0
Matthijs van der Wild <matthijs@vanderwild.com>
2025-08-22 18:29:40 -0400

    Revert __init__.py

---
+++ src/derivkit/__init__.py
@@ -0,0 +1,29 @@
+from .adaptive_fit import AdaptiveFitDerivative
+from .finite_difference import FiniteDifferenceDerivative
+from .forecast_kit import ForecastKit
+from .kit import DerivativeKit
+from .plotutils.plot_helpers import PlotHelpers
+from .plotutils.plot_kit import PlotKit
+from .utils import (
+    central_difference_error_estimate,
+    generate_test_function,
+    is_finite_and_differentiable,
+    is_symmetric_grid,
+    log_debug_message,
+    normalize_derivative,
+)
+
+__all__ = [
+    "AdaptiveFitDerivative",
+    "FiniteDifferenceDerivative",
+    "DerivativeKit",
+    "PlotHelpers",
+    "PlotKit",
+    "log_debug_message",
+    "is_finite_and_differentiable",
+    "normalize_derivative",
+    "central_difference_error_estimate",
+    "is_symmetric_grid",
+    "generate_test_function",
+    "ForecastKit"
+]
