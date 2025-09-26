9a5efc1234ce522dc49cd9524073f87472612e28
niko.sarcevic <nikolina.sarcevic@gmail.com>
2025-09-26 08:18:58 -0400

    structure(#89): scaffold forecasting/ (expansions, calculus, likelihoods) as stubs

---
+++ src/derivkit/forecasting/expansions.py
@@ -0,0 +1,4 @@
+"""Likelihood expansions (Fisher, DALI, biases)."""
+def fisher(*args, **kwargs): raise NotImplementedError
+def dali(*args, **kwargs): raise NotImplementedError
+def fisher_bias(*args, **kwargs): raise NotImplementedError
